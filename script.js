f27f8e647cd872536dedf4214f82390972c381b2
omarfaruk70 <faruk1611281892@gmail.com>
2022-09-26 02:45:37 +0600

    project done

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ let taskadd = document.querySelector('#newTask');
 let taskFilter = document.querySelector('#task-filter');
 let allTask = document.querySelector('ul');
 let clearBtn = document.querySelector('#clearAll');
-
 // add eventlistener
 inputForm.addEventListener('submit', addTask);
 allTask.addEventListener('click', removeTask);
 clearBtn.addEventListener('click', clearAll)
-
+taskFilter.addEventListener('keyup', filtertask)
 // define function
-function addTask(e){
+function addTask(add){
     if(taskadd.value === ''){
         alert('Add a task')
     }
@@ -25,16 +24,28 @@ function addTask(e){
         li.appendChild(link)
         taskadd.value = '';
     }
-    e.preventDefault();
+    add.preventDefault();
 };
-function removeTask(e){
-   if(e.target.hasAttribute('href')){
-    let element = e.target.parentElement;
+function removeTask(all){
+   if(all.target.hasAttribute('href')){
+    let element = all.target.parentElement;
     element.remove();
    }
 };
-function clearAll(e) {
-   if(confirm('Are you sure?')){
+function clearAll(clr) {
+   if(confirm(' Opps! Are you sure?')){
     allTask.innerHTML = '';    
 }
+};
+function filtertask(e){
+    let text = e.target.value.toLowerCase();
+    document.querySelectorAll('li').forEach((task)=>{
+        let item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block'
+        }
+        else{
+            task.style.display = 'none'
+        };
+    })
 };
